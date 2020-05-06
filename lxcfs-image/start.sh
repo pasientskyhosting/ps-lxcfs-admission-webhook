ad2413ac4483edd6d1d2e60a349cb998fd67d75e
Chad Jones <cj@patientsky.com>
2020-05-06 14:35:51 +0200

    Make it work with CoreOS

--- lxcfs-image/start.sh
+++ lxcfs-image/start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Cleanup
 nsenter -m/proc/1/ns/mnt fusermount -u /var/lib/lxcfs 2> /dev/null || true
 nsenter -m/proc/1/ns/mnt [ -L /etc/mtab ] || \
@@ -10,8 +9,9 @@ mkdir -p /usr/local/lib/lxcfs /var/lib/lxcfs
 
 # Update lxcfs
 cp -f /lxcfs/lxcfs /usr/local/bin/lxcfs
-cp -f /lxcfs/liblxcfs.so /usr/local/lib/lxcfs/liblxcfs.so
-
+cp -f /lxcfs/liblxcfs.so /lxcfs/lib64/liblxcfs.so
+cp -n /usr/lib64/libfuse.so.2.9.2 /lxcfs/lib64/libfuse.so.2
+cp -n /usr/lib64/libulockmgr.so.1.0.1 /lxcfs/lib64/libulockmgr.so.1
 
 # Mount
-exec nsenter -m/proc/1/ns/mnt /usr/local/bin/lxcfs /var/lib/lxcfs/
+exec nsenter -m/proc/1/ns/mnt /opt/bin/lxcfs /var/lib/lxcfs/
